4747492f36f5564a48836212049347a4047fd7c0
boen <boengood@gmail.com>
2012-08-27 16:47:24 +0800

    new file:   boen-biz/pom.xml 	new file:   boen-biz/src/main/java/com/boen/App.java 	new file:   boen-biz/src/test/java/com/boen/AppTest.java 	new file:   boen-model/pom.xml 	new file:   boen-model/src/main/java/com/boen/App.java 	new file:   boen-model/src/test/java/com/boen/AppTest.java 	new file:   boen-web/pom.xml 	new file:   boen-web/src/main/webapp/WEB-INF/web.xml 	new file:   boen-web/src/main/webapp/index.jsp 	new file:   pom.xml

---
+++ boen-biz/src/main/java/com/boen/App.java
@@ -0,0 +1,13 @@
+package com.boen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
